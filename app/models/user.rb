35eb089ce4b604f8bd84218378e40d9cf05447de
David Virtser <poison77@gmail.com>
2014-08-06 09:27:15 +0300

    added more tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ActiveRecord::Base
   belongs_to :organization
 
-  validates :name, presence: true,
-            :length => {:within => 5..80}
-  validates :email, :presence => true,
-            :email => true
-  validates :phone, presence: true,
-            :length => {:within => 9..20}
-  validates :password, :presence => true,
-            :length => {:within => 6..12}
+  validates :name, presence: true, length: 5..80
+  validates :email, presence: true, email: true
+  validates :phone, presence: true, length: 9..20
+  validates :password, :presence => true, length: 6..12
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,23 +14,62 @@ describe User do
 
   it { should be_valid }
 
+  # Name tests
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
 
+  describe "when name is too short" do
+    before { @user.name = "abc" }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm" }
+    it { should_not be_valid }
+  end
+
+  # Email tests
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
 
+  describe "when email is not correct" do
+    before { @user.email = "a@a" }
+    it { should_not be_valid }
+  end
+
+  # Phone tests
   describe "when phone is not present" do
     before { @user.phone = " " }
     it { should_not be_valid }
   end
 
+  describe "when phone is too short" do
+    before { @user.phone = "12345" }
+    it { should_not be_valid }
+  end
+
+  describe "when phone is too long" do
+    before { @user.phone = "123456789012345678901" }
+    it { should_not be_valid }
+  end
+
+  # Password tests
   describe "when password is not present" do
     before { @user.password = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password is too short" do
+    before { @user.password = "12345" }
+    it { should_not be_valid }
+  end
+
+  describe "when password is too long" do
+    before { @user.password = "1234567890123" }
     it { should_not be_valid }
   end
 end
