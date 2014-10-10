b181da8599737e98a2ad9e2ce123b48d1f70ca71
David Virtser <dvirtser@google.com>
2014-10-10 13:40:27 +0300

    added sms message receive log

--- app/controllers/receive_controller.rb
+++ app/controllers/receive_controller.rb
@@ -5,16 +5,23 @@ class ReceiveController < ApplicationController
   def index
     message_sid = params[:Sid]
     message_date = params[:SentDate]
-    from_phone = params[:From]
-    to_phone = params[:To]
-    body - params[:Body]
-    status - params[:Status]
+    from_phone_number = params[:From]
+    to_phone_number = params[:To]
+    body = params[:Body]
+    status = params[:Status]
 
-    save_message(message_sid, message_date, from_phone_number, to_phone_number, body, status)
+    save_received_message_log(message_sid, message_date, from_phone_number, to_phone_number, body, status)
   end
 
-  def save_message(message_sid, message_date, from_phone, to_phone, body, status)
-
-  end
-
+  def save_received_message_log(message_sid, message_date, from_phone, to_phone, body, status)
+     @sent_message_log = MessageReceive.new(
+                                          sid: message_sid,
+                                          date: message_date,
+                                          from_phone: from_phone,
+                                          to_phone: to_phone,
+                                          body: body,
+                                          status: status
+                                        )
+     @sent_message_log.save
+   end
 end
