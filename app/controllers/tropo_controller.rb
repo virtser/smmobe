48f7c646f7af8dee692552e79253bc214c00fce2
David Virtser <poison77@gmail.com>
2014-10-27 07:35:33 +0200

    initiate call to tropo

--- app/controllers/tropo_controller.rb
+++ app/controllers/tropo_controller.rb
@@ -7,9 +7,14 @@ class TropoController < ApplicationController
   # POST /tropo.json
   def index
 
+    data = JSON.parse(request.raw_post)
+    numbertodial = [:session][:parameters][:numbertodial]
+    msg = data[:session][:parameters][:msg]
+    id = [:session][:parameters][:customername]
+
     t = Tropo::Generator.new()
-    t.call(:from => PROD_PHONE_NUMBER, :to => '+972544472571', :network => "SMS", :name => '999')
-    t.say(:value => 'test', :name => '999')
+    t.call(:from => PROD_PHONE_NUMBER, :to => '+' + numbertodial, :network => 'SMS', :name => id)
+    t.say(:value => msg, :name => id)
 
     render :json =>  t.response
 
