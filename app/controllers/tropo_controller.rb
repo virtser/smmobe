706c8e30141887eaf169b1354d2bd582db398694
David Virtser <dvirtser@google.com>
2014-10-27 22:25:15 +0200

    bug fixes

--- app/controllers/tropo_controller.rb
+++ app/controllers/tropo_controller.rb
@@ -13,19 +13,13 @@ class TropoController < ApplicationController
       logger.info request.raw_post
 
       data = JSON.parse(request.raw_post)
-      logger.info "JSON parsed response data: " + data
 
-      numbertodial = "+" + data[:session][:parameters][:numbertodial]
-      logger.info "JSON numbertodial: " + numbertodial.to_s
-
+      numbertodial = data[:session][:parameters][:numbertodial]
       msg = data[:session][:parameters][:msg]
-      logger.info "JSON msg: " + msg.to_s
-
       id = data[:session][:parameters][:customername]
-      logger.info "JSON id: " + id.to_s
 
       t = Tropo::Generator.new()
-      t.call(:from => PROD_PHONE_NUMBER, :to => numbertodial, :network => 'SMS', :name => id)
+      t.call(:from => PROD_PHONE_NUMBER, :to => "+" + numbertodial, :network => 'SMS', :name => id)
       t.say(:value => msg, :name => id)
 
     rescue Exception => error_msg
