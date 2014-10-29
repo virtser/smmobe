7042c4a0fbe4207d617ded3a17da634d1d173757
David Virtser <dvirtser@google.com>
2014-10-29 22:18:29 +0200

    testing tropo responses

--- app/controllers/tropo_controller.rb
+++ app/controllers/tropo_controller.rb
@@ -18,25 +18,41 @@ class TropoController < ApplicationController
         logger.info "=== GOT POST REQUEST FROM TROPO FOR RESPOND ON SMS MESSAGE ==="
 
         #answer = data[:result][:actions][:color][:value]
-        t.say(:value => "Thank you!")
+        #numbertodial = data['result']['calledid']
+        #t.call(:from => PROD_PHONE_NUMBER, :to => "+" + numbertodial, :network => 'SMS')
+        #t.say(:value => "Thank you!")
 
       else
 
-        logger.info "=== GOT POST REQUEST FROM TROPO TO SEND SMS MESSAGE ==="
+        if (data['session']['initialText'].nil?)
+
+          logger.info "=== GOT POST REQUEST FROM TROPO TO SEND SMS MESSAGE ==="
 
 
-        numbertodial = data['session']['parameters']['numbertodial']
-        msg = data['session']['parameters']['msg']
-        id = data['session']['parameters']['customername']
+          numbertodial = data['session']['parameters']['numbertodial']
+          msg = data['session']['parameters']['msg']
+          id = data['session']['parameters']['customername']
 
-        t.call(:from => PROD_PHONE_NUMBER, :to => "+" + numbertodial, :network => 'SMS', :name => id)
+          t.call(:from => PROD_PHONE_NUMBER, :to => "+" + numbertodial, :network => 'SMS', :name => id)
 
-        t.ask :name => 'color',
-              :timeout => 60,
-              :say => {:value => "What's your favorite color?  Choose from red, blue or green."},
-              :choices => {:value => "red, blue, green"}
+          t.ask :name => 'color',
+                :timeout => 60,
+                :say => {:value => "What's your favorite color?  Choose from red, blue or green."},
+                :choices => {:value => "red, blue, green"}
 
-        t.on :event => 'continue', :next => '/tropo?continue=true'
+          t.on :event => 'continue', :next => '/tropo?continue=true'
+
+
+        else
+
+          logger.info "=== GOT POST REQUEST FROM TROPO ON SMS MESSAGE REPLY ==="
+
+          numbertodial = data['session']['from']['id']
+          answer = data['session']['initialText']
+          t.call(:from => PROD_PHONE_NUMBER, :to => "+" + numbertodial, :network => 'SMS')
+          t.say(:value => "You answered: " + answer)
+
+        end
 
       end
 
