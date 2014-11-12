7ef5b5c1cf3fa3bcfdcdd82b5617dccd7e2c3f60
David Virtser <dvirtser@google.com>
2014-11-12 21:46:16 +0200

    changed to 72h

--- app/controllers/receive_controller.rb
+++ app/controllers/receive_controller.rb
@@ -17,7 +17,7 @@ class ReceiveController < ApplicationController
       body = params[:text]
       status = params[:type]
       user_id = User.where(phone: to_phone_number).limit(1).pluck(:id)[0]
-      campaign_id = Campaign.where("sent_from_phone = '" + to_phone_number + "' AND updated_at + interval '2' day  > now()").limit(1).pluck(:id)[0]
+      campaign_id = Campaign.where("sent_from_phone = '" + to_phone_number + "' AND updated_at + interval '3' day  > now()").limit(1).pluck(:id)[0]
       save_received_message_log(message_sid, from_phone_number, to_phone_number, body, status, campaign_id, user_id)
 
       # TODO: reply if body contains defined variable to reply.
