5160cde52d872f967b5c119ff27c620530b15799
David Virtser <dvirtser@google.com>
2014-11-15 22:55:29 +0200

    bug fixes

--- app/controllers/receive_controller.rb
+++ app/controllers/receive_controller.rb
@@ -19,8 +19,8 @@ class ReceiveController < ApplicationController
       user_id = User.where(phone: to_phone_number).limit(1).pluck(:id)[0]
 
       # possible campaign IDs to assign the message to
-      campaign_id = Campaign.where("isdisabled = false AND user_id = (?) AND campaigns.updated_at + interval '?' day  > now()", user_id, Generic.get_campaign_run_interval)
-                             .joins("LEFT OUTER JOIN customers ON campaigns.id = customers.campaign_id AND customers.phone = '" + from_phone_number + "'")
+      campaign_id = Campaign.where("isdisabled = false AND user_id = (?) AND campaign_status_id = 2 AND campaigns.updated_at + interval '?' day  > now()", user_id, Generic.get_campaign_run_interval)
+                             .joins("JOIN customers ON campaigns.id = customers.campaign_id AND customers.phone = '" + from_phone_number + "'")
                              .limit(1).pluck("campaigns.id")[0]
 
       save_received_message_log(message_sid, from_phone_number, to_phone_number, body, status, campaign_id, user_id)
