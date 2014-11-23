3dab18e0d3bd571a32c579875350a9a2dbc81451
David Virtser <dvirtser@google.com>
2014-11-23 23:49:57 +0200

    phone extended to 20 chars

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,6 @@ class Customer < ActiveRecord::Base
   belongs_to :campaign
 
   #validates :first_name, presence: true, length: 2..80
-  validates :phone, :numericality => {:only_integer => true}, presence: true, length: 9..15
+  validates :phone, :numericality => {:only_integer => true}, presence: true, length: 9..20
 
 end
