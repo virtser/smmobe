5800e672ec1d96887dcd85aa292ee8383270a02e
David Virtser <dvirtser@google.com>
2014-11-24 23:32:54 +0200

    disabled images precompiling

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SMMobe
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.enabled = true
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    #config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.assets.paths << Rails.root.join("app", "assets", "files")
 
   end
