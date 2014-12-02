0c7ec55956a3a79e4c4d44adad47145e86c3f62e
David Virtser <dvirtser@google.com>
2014-12-02 23:35:02 +0200

    unsubscribe changed from 0 to unsubscribe

--- app/controllers/receive_controller.rb
+++ app/controllers/receive_controller.rb
@@ -65,7 +65,7 @@ class ReceiveController < ApplicationController
 
   def process_reply(message_sid, from_phone_number, to_phone_number, body, status, campaign_id, user_id)
       
-      if body.include? "0"  # unsubscribe
+      if body.include? "unsubscribe"  # unsubscribe
         unsubscribe_customer(from_phone_number, user_id)        
       end 
 
