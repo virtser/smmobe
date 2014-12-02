f1657d43ec8616dee3b23ca3793f94ddd010ff41
David Virtser <dvirtser@google.com>
2014-12-02 22:41:31 +0200

    deploy script improved

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-STAGE_BRANCH="develop:master"
-PROD_BRANCH="master"
+APPNAME="smmobe"
+BRANCH="master"
 
 if [ $1 ]; then
 	ENV=$1
@@ -11,34 +11,41 @@ if [ $1 ]; then
 		exit 1
 	fi
 
+	if [ $ENV == 'stage' ]; then
+		APPNAME="smmobe-staging"
+		BRANCH="develop:master"
+	fi
+
 	if [ $2 ]; then
-		STAGE_BRANCH=$2
-		PROD_BRANCH=$2
+		BRANCH=$2
 	fi
 
 	echo "============================= Deployment $1 Started ============================="
 	
-	echo "Run linting to check for errors"
+	echo "--- Run linting to check for errors ---"
 	rubocop --fail-fast --fail-level 'E'
 
 	# TODO: run tests
 
 	if [ $? == 1 ]; then
 		echo " Exiting as found compiation errors. "
-	else
-		if [ $ENV == 'stage' ]; then
-			echo "Deploying to Heroku"
-			git push staging $STAGE_BRANCH
+		exit 1
+	fi
 
-			echo "Running DB migration"
-			heroku run rake db:migrate --app smmobe-staging
-		else
-			echo "Deploying to Heroku"
-			git push heroku $PROD_BRANCH
-			
-			echo "Running DB migration"
-			heroku run rake db:migrate --app smmobe
-		fi
+	echo "--- Deploying to Heroku ---"
+	git push -v staging $BRANCH
+
+	if [ $? == 1 ]; then
+		echo " Exiting as deployment failed. "
+		exit 1
+	fi
+
+	echo "-- Running DB migration ---"
+	heroku run rake db:migrate --app $APPNAME
+
+	if [ $? == 1 ]; then
+		echo " Exiting as db migration failed. "
+		exit 1
 	fi
 
 	echo "============================= Deployment $1 Finished ============================="
